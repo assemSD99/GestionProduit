6377cb09ef74e4c76fac0f8849db4761bdce9988
assemSD99 <assem.oujda@gmail.com>
2024-01-13 16:52:54 +0100

    Initialisation du projet

---
+++ src/main/java/com/assem/Produit.java
@@ -0,0 +1,4 @@
+package com.assem;
+
+public class Produit {
+}
---
+++ src/test/java/Metier/produitServiceTest.java
@@ -0,0 +1,10 @@
+package Metier;
+
+import static org.junit.Assert.*;
+
+public class produitServiceTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+}
