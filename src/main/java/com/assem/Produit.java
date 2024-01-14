8d3901b5e6d015b3e7b4478dbd1785a6ea27d7b0
assemSD99 <assem.oujda@gmail.com>
2024-01-14 14:31:04 +0100

    Remplir la class Produit

--- src/main/java/com/assem/Produit.java
+++ src/main/java/com/assem/Produit.java
@@ -1,4 +1,52 @@
 package com.assem;
 
 public class Produit {
+    private Long id;
+    private String nom;
+    private double prix;
+    private int quantite;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                ", quantite=" + quantite +
+                '}';
+    }
 }
